3c7b3ca2620567abe178bd8e90dc595a7bfb9033
juancarlosmiranda <juancarlos.miranda@uc.edu.py>
2020-11-15 23:16:43 +0100

    second commit

---
+++ ClassifierMainDL.m
@@ -0,0 +1,32 @@
+classdef ClassifierMainDL   
+    % Author: https://github.com/juancarlosmiranda/
+    % 
+    %DeepLearningClassifier This is a main process
+    %   Here this is an detailed explainded text
+    % Based on Mathworks Tutorial "Classify Webcam Images Using Deep Learning"
+    % https://es.mathworks.com/help/deeplearning/ug/classify-images-from-webcam-using-deep-learning.html?s_tid=srchtitle
+    % 
+    % I adapted this to OOP format, is a practice to develop code for
+    % Deep Learning. It uses a pre-trained Alexnet to classify objects.
+    % 
+    % Run it with ->
+    % c = DeepLearningClassifier;
+    % c.runner()    
+    
+    properties
+        Property1;
+    end    
+    % ----------------------
+    methods(Static)
+        function Main()
+            clc, clear all, close all;
+            fprintf('\n -------------------------------- \n');
+            fprintf('MAIN METHOD');
+            fprintf('\n -------------------------------- \n');
+            classifier_real_time = DeepLearningClassifier;
+            classifier_real_time.runner;
+        end        
+    end
+end
+
+    
